24269cb8c856e9149afb7d0d94f64fad79f68d79
shumbum <robert_shum@hotmail.com>
2014-02-12 23:30:21 -0800

    First edit!  Woo!

---
+++ MyFirstApp/src/com/example/myfirstapp/DisplayMessageActivity.java
@@ -0,0 +1,48 @@
+package com.example.myfirstapp;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v4.app.NavUtils;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class DisplayMessageActivity extends Activity {
+
+    @SuppressLint("NewApi")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_message);
+
+        // Make sure we're running on Honeycomb or higher to use ActionBar APIs
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+            // Show the Up button in the action bar.
+            getActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+        
+        //get intent and message from it
+        Intent intent = getIntent();
+        String msg = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        
+        //Create text view
+        TextView textView = new TextView(this);
+        textView.setTextSize(40);
+        textView.setText(msg);
+        
+        //set text view as activity layout
+        setContentView(textView);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+        case android.R.id.home:
+            NavUtils.navigateUpFromSameTask(this);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
